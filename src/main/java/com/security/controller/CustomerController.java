bddb2a56db755f5e13f7578ab05b8c1e6729c910
Nuri Korganci <nkorganci@gmail.com>
2022-03-03 23:11:37 -0600

    m

--- src/main/java/com/security/controller/CustomerController.java
+++ src/main/java/com/security/controller/CustomerController.java
@@ -5,20 +5,28 @@ import com.security.repository.CustomerRepository;
 import com.security.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("customer")
+//@RequestMapping("customer")
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
     //1 POST a customer
-    @PostMapping("/add")
-    public Customer addCustomerController(@RequestBody Customer customer){
+    @PostMapping("/customers")
+    public List<Customer> addCustomerController(@RequestBody List<Customer> customer){
 
         return customerService.addCustomerService(customer);
+    }
+    // 2 GET mapping
+    @GetMapping("/customers")
+    public List<Customer> getCustomers(){
+        return customerService.showAllCustomers();
     }
 }
--- src/main/java/com/security/service/CustomerService.java
+++ src/main/java/com/security/service/CustomerService.java
@@ -5,14 +5,21 @@ import com.security.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
     //1 POST a Customer
-    public Customer addCustomerService(Customer customer){
-        return customerRepository.save(customer);
+    public List<Customer> addCustomerService(List<Customer> customer){
+        return customerRepository.saveAll(customer);
+    }
+
+    // 2 Get customer
+    public List<Customer> showAllCustomers(){
+        return customerRepository.findAll();
     }
 
 
